45cdddb0439d680c68f6eee10c92c78ffd633c84
Satrya Sabeni <s.sabeni17@gmail.com>
2015-08-11 16:49:53 +0200

    Tweet opgehaald, backup voor cross array blog items..

--- page-blog.php
+++ page-blog.php
@@ -18,8 +18,20 @@ $data['page'] = new TimberPost();
 
 
 // Blog
+
+//var_dump(get_posts(array('post_type'=> array('tweet','post'))));//
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$data['posts'] = Timber::get_posts('post_type=post&post_status=publish&order=DESC&posts_per_page=6&paged=' . $paged);//&order=ASC&posts_per_page=1&paged=' . $paged);
+$blog_items = array(
+	'post_type'=> array('tweet','post'),
+	'paged' => $paged,
+	'posts_per_page'=> 6,
+	'status' => 'publish',
+	'order' => 'DESC',
+	'orderby' => 'rand'
+);
+$data['posts'] = Timber::get_posts($blog_items);
+$data['current_page'] = get_query_var('paged');
+
 
 // Quote's
 $data['quote'] = Timber::get_post('post_type=quotes&post_status=publish&orderby=rand');
