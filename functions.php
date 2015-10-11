5920196cf0feb54bca0173d70e42ef528f7cdeb9
Satrya Sabeni <s.sabeni17@gmail.com>
2015-10-11 23:54:12 +0200

    added google font corrects way.....

--- functions.php
+++ functions.php
@@ -298,10 +298,3 @@ function my_mce_before_init( $settings ) {
     return $settings;
 
 }
-
-function wpb_add_google_fonts() {
-
-wp_enqueue_style( 'wpb-google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,700,300', false );
-}
-
-add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
